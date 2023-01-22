a4b76100e4f63365910406c55002a06bb5f30612
Jens Davidsen <jensimik@users.noreply.github.com>
2023-01-22 23:29:04 +0100

    style menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,25 +3,24 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="flex main">
-    <div class="full">
-      <nav class="imponent">
-        <a href="#" class="brand">
-          <img class="logo" src="@/assets/fist.png" />
-          <span></span>
-        </a>
+  <nav class="imponent">
+    <a href="#" class="brand">
+      <img class="logo" src="@/assets/fist.png" />
+      <span></span>
+    </a>
 
-        <!-- responsive-->
-        <input id="bmenub" type="checkbox" class="show">
-        <label for="bmenub" class="burger pseudo button">&#8801;</label>
+    <!-- responsive-->
+    <input id="bmenub" type="checkbox" class="show">
+    <label for="bmenub" class="burger pseudo button">&#8801;</label>
 
-        <div class="menu">
-          <router-link :to="{ name: 'home' }" class="pseudo button icon-picture">Home</router-link>
-          <router-link :to="{ name: 'about' }" class="button icon-puzzle">+problem</router-link>
-        </div>
-      </nav>
+    <div class="menu">
+      <router-link :to="{ name: 'home' }" class="pseudo button icon-picture">Home</router-link>
+      <router-link :to="{ name: 'about' }" class="button icon-puzzle">+problem</router-link>
     </div>
+  </nav>
 
+  <div class="flex main">
+    <div class="full paddypower"></div>
     <RouterView />
   </div>
 </template>
@@ -34,10 +33,23 @@ div.main {
 }
 
 nav.imponent {
-  position: relative;
-  margin: 0;
+  max-width: 800px;
+  margin-left: auto;
+  margin-right: auto;
   box-shadow: none;
   border-bottom: 4px dotted #dfdfdf;
+}
+
+div.paddypower {
+  padding-top: 2.5em;
+}
+
+.logo {
+  height: 4.5em;
+}
+
+a.brand {
+  padding-left: 0;
 }
 </style>
 <style>
