ed028374d59bb415552a8d678a326e7453c2450a
Jens Davidsen <jensimik@users.noreply.github.com>
2023-01-22 15:28:50 +0100

    more compact mobile view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -7,21 +7,17 @@ import TheWelcome from '../components/TheWelcome.vue'
     <table class="primary">
       <thead>
         <tr>
-          <th></th>
-          <th>Today</th>
-          <th>Tomorrow</th>
+          <th colspan="2">Hours</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td>Opening hours</td>
+          <td>Today</td>
           <td>09:00-23:30</td>
-          <td>10:00-21:00</td>
         </tr>
         <tr>
-          <td>Calendar</td>
-          <td>-</td>
-          <td>18:00-19:30 junior kursus<br>19:30-20:00 letøvet kursus</td>
+          <td>Tomorrow</td>
+          <td>10:00-21:00</td>
         </tr>
       </tbody>
     </table>
@@ -30,15 +26,34 @@ import TheWelcome from '../components/TheWelcome.vue'
     <table class="primary">
       <thead>
         <tr>
-          <th>Gym load</th>
-          <th>Currently</th>
-          <th>Today</th>
+          <th colspan="2">Calendar</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td>People</td>
+          <td>18:00-19:30</td>
+          <td>junior kursus</td>
+        </tr>
+        <tr>
+          <td>19:30-20:00</td>
+          <td>letøvet kursus</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <table class="primary">
+      <thead>
+        <tr>
+          <th colspan="2">Gym load</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Currently</td>
           <td>15</td>
+        </tr>
+        <tr>
+          <td>Today</td>
           <td><img
               src="https://i0.wp.com/wonderoftech.com/wp-content/uploads/2020/06/Live-Visit-Data-versus-Popular-Times-Google-Maps.jpg?w=556&ssl=1"
               width="128" />
@@ -50,30 +65,28 @@ import TheWelcome from '../components/TheWelcome.vue'
     <table class="primary">
       <thead>
         <tr>
-          <th>Feed/news</th>
-          <th></th>
-          <th></th>
+          <th colspan="3">Feed/news</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td>2023-01-22</td>
-          <td>Stökt</td>
+          <td>01-22</td>
+          <td>S</td>
           <td>Planned expiration 7a+</td>
         </tr>
         <tr>
-          <td>2023-01-20</td>
-          <td>Stökt</td>
+          <td>01-20</td>
+          <td>S</td>
           <td>One Jump 7a</td>
         </tr>
         <tr>
-          <td>2023-01-18</td>
-          <td>Section1</td>
+          <td>01-18</td>
+          <td>S1</td>
           <td>Blue problem 5c</td>
         </tr>
         <tr>
-          <td>2023-01-16</td>
-          <td>Section1</td>
+          <td>01-16</td>
+          <td>S2</td>
           <td>Red problem 6c+</td>
         </tr>
       </tbody>
