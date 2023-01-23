ea34ff388ab10dc48151f88e35b8240f368fd780
Jens Davidsen <jensimik@users.noreply.github.com>
2023-01-23 01:12:49 +0100

    .

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -11,7 +11,7 @@ import Calendar from '../components/Calendar.vue'
       <Hours></Hours>
       <Calendar></Calendar>
       <HoursPopular></HoursPopular>
-      <h2>Strip</h2>
+      <h2>Section4 strip 4 feb <span class="small">8 days left to close the set</span></h2>
       <div><img src="@/assets/sections.png" style="width:100%" /></div>
       <Feed></Feed>
     </div>
