833fd86d327b1ef8bd62b46ba8090c63ed6fc2a1
Jens Davidsen <jensimik@users.noreply.github.com>
2023-01-24 13:22:42 +0100

    .

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -15,15 +15,15 @@ hours.value = await HourMethodsAPI.index();
                 href="https://kulturogfritidn.kk.dk/noerrebrohallen">nørrebrohallen</a>)</span></h2>
 
     <div class="flex two">
-        <div class="fourth">{{ hours.hours_today }}</div>
-        <div class="three-fourth">Opening hours today (2023-01-24)</div>
+        <div class="third">{{ hours.hours_today }}</div>
+        <div class="two-third">hours today (2023-01-24)</div>
     </div>
 
     <div class="flex two" v-for="item in items" v-if="items.length">
-        <div class="fourth">{{ item.time }}</div>
-        <div class="three-fourth">{{ item.title }}</div>
+        <div class="third">{{ item.time }}</div>
+        <div class="two-third">{{ item.title }}</div>
     </div>
     <div v-else class="flex">
-        <div class="three-fourth off-fourth">empty klubmodul calendar</div>
+        <div class="two-third off-third">empty klubmodul calendar</div>
     </div>
 </template>
