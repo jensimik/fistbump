39d745bcc4ff501cb8e6d6f68e957f624fcefae9
Jens Davidsen <jensimik@users.noreply.github.com>
2023-01-27 19:53:31 +0100

    Update GradeDistribution.vue

--- frontend/src/components/GradeDistribution.vue
+++ frontend/src/components/GradeDistribution.vue
@@ -3,16 +3,40 @@ import GradeDistributionMethodsAPI from '../api/resources/GradeDistributionMetho
 import { GChart } from 'vue-google-charts';
 import { ref } from 'vue';
 
-const type = 'ColumnChart';
+const type = 'BarChart';
 
 const options = {
-    chartArea: { width: '100%' },
+    width: '100%',
+    height: '100%',
+    chartArea: { left: '0', top: 0, width: "100%", height: '100%' },
+    bar: { groupWidth: '100%' },
     isStacked: "percent",
     legend: { position: "none" },
     series: {
         0: {
-            color: '#0074d9'
+            color: '#2ECC40',
         },
+        1: {
+            color: '#FFDC00',
+        },
+        2: {
+            color: '#0074d9',
+        },
+        3: {
+            color: '#B10DC9',
+        },
+        4: {
+            color: '#FF4136',
+        },
+        5: {
+            color: '#85144b',
+        },
+        6: {
+            color: '#111111'
+        },
+        7: {
+            color: '#fff'
+        }
     },
     backgroundColor: 'transparent',
     hAxis: {
@@ -34,18 +58,27 @@ const options = {
 
 const distribution = ref({});
 distribution.value = await GradeDistributionMethodsAPI.index();
-
-const data = [
+const data_boulder = [
     ['Type', 'green', 'yellow', 'blue', 'purple', 'red', 'brown', 'black', 'white', { role: 'annotation' }],
-    ['Boulder', distribution.value.boulder.green, distribution.value.boulder.yellow, distribution.value.boulder.blue, distribution.value.boulder.purple, distribution.value.boulder.red, distribution.value.boulder.brown, distribution.value.boulder.black, distribution.value.boulder.white],
-    ['Stökt', distribution.value.stokt.green, distribution.value.stokt.yellow, distribution.value.stokt.blue, distribution.value.stokt.purple, distribution.value.stokt.red, distribution.value.stokt.brown, distribution.value.stokt.black, distribution.value.stokt.white]
-]
-
-console.log(distribution.value)
+    ['Boulder', distribution.value.boulder.green, distribution.value.boulder.yellow, distribution.value.boulder.blue, distribution.value.boulder.purple, distribution.value.boulder.red, distribution.value.boulder.brown, distribution.value.boulder.black, distribution.value.boulder.white, ''],
+];
+const data_stokt = [
+    ['Type', 'green', 'yellow', 'blue', 'purple', 'red', 'brown', 'black', 'white', { role: 'annotation' }],
+    ['Stökt', distribution.value.stokt.green, distribution.value.stokt.yellow, distribution.value.stokt.blue, distribution.value.stokt.purple, distribution.value.stokt.red, distribution.value.stokt.brown, distribution.value.stokt.black, distribution.value.stokt.white, ''],
+];
 </script>
 
 <template>
     <h2>Grade distribution <span class="small">()</span>
     </h2>
-    <GChart id="ccc" :data="data" :options="options" :type="type"></GChart>
+    <h3>Boulder</h3>
+    <GChart class="cc" :data="data_boulder" :options="options" :type="type"></GChart>
+    <h3>Stökt</h3>
+    <GChart class="cc" :data="data_stokt" :options="options" :type="type"></GChart>
 </template>
+
+<style scoped>
+.cc {
+    height: 2em;
+}
+</style>
