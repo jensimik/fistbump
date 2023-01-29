c4ded87bba8fdd1f344bb92b4d77973c839a40a0
Jens Davidsen <jensimik@users.noreply.github.com>
2023-01-29 13:27:49 +0100

    Update HomeView.vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -78,7 +78,7 @@ window.addEventListener('visibilitychange', function () {
           </dl>
         </dd>
       </dl>
-      <button @click="dismiss">dismiss welcome message</button>
+      <button @click="dismiss">dismiss welcome message 👊</button>
     </div>
     <Calendar v-if="sections.includes('calendar')"></Calendar>
     <HoursPopular v-if="sections.includes('hours_popular')"></HoursPopular>
