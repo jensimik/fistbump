4dfdb8bdb48084cfb76dcbd57d50221f72ac5633
Jens Davidsen <jensimik@users.noreply.github.com>
2023-01-29 13:03:10 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VitePWA({
-    registerType: 'autoUpdate',
+    // registerType: 'autoUpdate',
     workbox: {
       globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg}'],
       runtimeCaching: [
