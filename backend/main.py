febd9f806dbc8e9e4260185612aa2290e4789367
Jens Davidsen <jensimik@users.noreply.github.com>
2023-01-30 04:29:23 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -365,7 +365,7 @@ def webp_image(hex: str, new_width: int):
     if not webp_filename.exists():
         with Image.open(jpg_filename) as im:
             width, height = im.size
-            new_height = new_width * height / width
+            new_height = int(new_width * height / width)
             im.resize((new_width, new_height))
             im.save(webp_filename, format="webp", method=6, quality=40)
     return FileResponse(webp_filename, media_type="image/webp")
