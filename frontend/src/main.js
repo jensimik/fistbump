000e840a6a09eb6ad25ee9d58381e596443fe29e
Jens Davidsen <jensimik@users.noreply.github.com>
2023-02-02 03:49:47 +0100

    disable sentry performance monitoring

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import * as Sentry from "@sentry/vue";
-import { BrowserTracing } from "@sentry/tracing";
+// import { BrowserTracing } from "@sentry/tracing";
 
 
 const app = createApp(App)
@@ -10,13 +10,13 @@ const app = createApp(App)
 Sentry.init({
     app,
     dsn: "https://0d0b3deb62cc41dbbbd53509f51b3162@o4504589908377600.ingest.sentry.io/4504589910736897",
-    integrations: [
-        new BrowserTracing({
-            routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-            tracePropagationTargets: ["localhost", "my-site-url.com", /^\//],
-        }),
-    ],
-    tracesSampleRate: 1.0,
+    // integrations: [
+    //     new BrowserTracing({
+    //         routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+    //         tracePropagationTargets: ["localhost", "my-site-url.com", /^\//],
+    //     }),
+    // ],
+    tracesSampleRate: 0.2,
 });
 
 app.use(router)
