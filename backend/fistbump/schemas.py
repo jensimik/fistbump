113d8d15f5d1d8616e48d62616769a3c73d8b3b4
Jens Davidsen <jensimik@users.noreply.github.com>
2023-02-04 04:52:35 +0100

    Update schemas.py

--- backend/fistbump/schemas.py
+++ backend/fistbump/schemas.py
@@ -60,7 +60,7 @@ class Problem(BaseModel):
     ]
     image_width: Optional[int] = None
     image_height: Optional[int] = None
-    annotations: AnyStr = None  # Optional[list[Circle]] = []
+    annotations: Optional[int] = []  # Optional[list[Circle]] = []
     # image_hex only for problems in the boulder
     image_hex: Optional[str] = None
     # paths only for stokt
