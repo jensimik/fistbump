bc70298608210964f672d44060d47c6937b8e727
Jens Davidsen <jensimik@users.noreply.github.com>
2023-02-04 04:06:41 +0100

    .

--- backend/fistbump/routers/problems.py
+++ backend/fistbump/routers/problems.py
@@ -57,7 +57,7 @@ async def create_problem(
     setter: str = Form(),
     image_width: int = Form(),
     image_height: int = Form(),
-    annotations: list[schemas.Circle] = Form(),
+    annotations: list = Form(),
     text: Optional[str] = Form(""),
     section: Literal["S1", "S2", "S3", "S4", "S5"] = Form(),
     _: APIKey = Depends(get_api_key),
