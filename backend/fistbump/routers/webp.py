22d5f40c9f1861b584106f2a0a01f15e8cf5b545
Jens Davidsen <jensimik@users.noreply.github.com>
2023-02-05 19:22:15 +0100

    webp cache-control

--- backend/fistbump/routers/webp.py
+++ backend/fistbump/routers/webp.py
@@ -5,6 +5,9 @@ from fistbump.config import settings
 
 router = APIRouter(tags=["misc"])
 
+# cache webp images for a year (max limit)
+headers = {"Cache-Control": "public, max-age: 31536000, smax-age: 31536000, immutable"}
+
 
 @router.get("/webp/{hex}.webp", response_class=FileResponse)
 def webp_image(hex: str):
@@ -15,7 +18,7 @@ def webp_image(hex: str):
             # rotate it before save as webp don't have the exif
             im = ImageOps.exif_transpose(im)
             im.save(webp_filename, format="webp", method=6, quality=40)
-    return FileResponse(webp_filename, media_type="image/webp")
+    return FileResponse(webp_filename, media_type="image/webp", headers=headers)
 
 
 @router.get("/webp/{hex}/{new_width}.webp", response_class=FileResponse)
@@ -30,4 +33,4 @@ def webp_image_new_width(hex: str, new_width: int):
             new_height = int(new_width * height / width)
             im.thumbnail((new_width, new_height))
             im.save(webp_filename, format="webp", method=6, quality=40)
-    return FileResponse(webp_filename, media_type="image/webp")
+    return FileResponse(webp_filename, media_type="image/webp", headers=headers)
