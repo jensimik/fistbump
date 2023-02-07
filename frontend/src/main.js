816f415b0a52b743bde878e3a8a824ba354a074e
Jens Davidsen <jensimik@users.noreply.github.com>
2023-02-07 16:59:47 +0100

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import * as Sentry from "@sentry/vue";
-// import { BrowserTracing } from "@sentry/tracing";
+import { BrowserTracing } from "@sentry/tracing";
 
 
 const app = createApp(App)
