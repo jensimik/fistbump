e5ce42207f0cb8f0d759bc855c61a4d9f1d1e74c
Jens <jensimik@users.noreply.github.com>
2023-02-07 17:09:34 +0100

    sw-check-on-visibilitychange (#30)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import * as Sentry from "@sentry/vue";
-// import { BrowserTracing } from "@sentry/tracing";
+import { BrowserTracing } from "@sentry/tracing";
 
 
 const app = createApp(App)
@@ -11,13 +11,13 @@ Sentry.init({
     app,
     dsn: "https://0d0b3deb62cc41dbbbd53509f51b3162@o4504589908377600.ingest.sentry.io/4504589910736897",
     environment: import.meta.env.PROD ? "production" : "development",
-    // integrations: [
-    //     new BrowserTracing({
-    //         routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-    //         tracePropagationTargets: ["localhost", "my-site-url.com", /^\//],
-    //     }),
-    // ],
-    // tracesSampleRate: 0.2,
+    integrations: [
+        new BrowserTracing({
+            routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+            tracePropagationTargets: ["localhost", "fb-api.gnerd.dk", /^\//],
+        }),
+    ],
+    tracesSampleRate: 0.2,
 });
 
 app.use(router)
