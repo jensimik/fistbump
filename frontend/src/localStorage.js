fbcf47d07b34033a991b08ecf5c87f9852da9058
Jens Davidsen <jensimik@users.noreply.github.com>
2023-02-08 18:06:43 +0100

    ,

--- frontend/src/localStorage.js
+++ frontend/src/localStorage.js
@@ -6,7 +6,12 @@ export const sections = useLocalStorage("sections", ["calendar", "hours_popular"
 export const setter = useLocalStorage("setter", "no");
 export const setter_auth = useLocalStorage("setter_auth", "");
 
+// problems filters
 export const filter_search_text = useLocalStorage("filter_search_text", "");
 export const filter_sections = useLocalStorage("filter_sections", ["S1", "S2", "S3", "S4", "S5", "Ö"]);
 export const filter_grades = useLocalStorage("filter_grades", ["green", "yellow", "blue", "purple", "red", "brown", "black"]);
 export const filter_gridview = useLocalStorage("filter_gridview", true);
+
+// recent problems filters
+export const filter_rp_sections = useLocalStorage("filter_rp_sections", ["B", "Ö"]);
+export const filter_rp_grades = useLocalStorage("filter_rp_grades", ["green", "yellow", "blue", "purple", "red", "brown", "black"]);
--- frontend/src/views/SettingsView.vue
+++ frontend/src/views/SettingsView.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue';
 import GradeDistribution from '../components/GradeDistribution.vue';
 import HoldStats from '../components/HoldStats.vue';
 import SetterMethods from '../api/resources/SetterMethods';
-import { name, setter_code, sections, setter, setter_auth } from '../localStorage';
+import { name, setter_code, sections, setter, setter_auth, filter_rp_grades, filter_rp_sections } from '../localStorage';
 
 const button_data = ref({
     disabled: false,
@@ -44,7 +44,57 @@ async function link_setter_code(e) {
                     section</span></label>
             <label class="labelblock" for="problems"><input type="checkbox" id="problems" name="problems"
                     value="problems" v-model="sections" /><span class="checkable">problems</span></label>
-            <label>Do you set problems?</label>
+            <label>Recent problems filter</label>
+            <div class="search_options">
+                <label><input type="checkbox" name="B" value="B" v-model="filter_rp_sections" /><span
+                        class="toggle button" :class="{
+                            fade_button: !filter_rp_sections.includes('B')
+                        }">Boulder</span></label>
+                <label><input type="checkbox" name="Ö" value="Ö" v-model="filter_rp_sections" /><span
+                        class="toggle button" :class="{
+                            fade_button: !filter_rp_sections.includes('Ö')
+                        }">Stökt</span></label>
+            </div>
+            <div class="search_options">
+                <label><input type="checkbox" name="green" value="green" v-model="filter_rp_grades" /><span
+                        class="toggle button" :class="{
+                            green: filter_rp_grades.includes('green'),
+                            fade_button: !filter_rp_grades.includes('green')
+                        }">green</span></label>
+                <label><input type="checkbox" name="yellow" value="yellow" v-model="filter_rp_grades" /><span
+                        class="toggle button" :class="{
+                            yellow: filter_rp_grades.includes('yellow'),
+                            fade_button: !filter_rp_grades.includes('yellow')
+                        }">yellow</span></label>
+                <label><input type="checkbox" name="blue" value="blue" v-model="filter_rp_grades" /><span
+                        class="toggle button" :class="{
+                            blue: filter_rp_grades.includes('blue'),
+                            fade_button: !filter_rp_grades.includes('blue')
+                        }">blue</span></label>
+                <label><input type="checkbox" name="purple" value="purple" v-model="filter_rp_grades" /><span
+                        class="toggle button" :class="{
+                            purple: filter_rp_grades.includes('purple'),
+                            fade_button: !filter_rp_grades.includes('purple')
+                        }">purple</span></label>
+                <label><input type="checkbox" name="red" value="red" v-model="filter_rp_grades" /><span
+                        class="toggle button" :class="{
+                            red: filter_rp_grades.includes('red'),
+                            fade_button: !filter_rp_grades.includes('red')
+                        }">red</span></label>
+                <label><input type="checkbox" name="brown" value="brown" v-model="filter_rp_grades" /><span
+                        class="toggle button" :class="{
+                            brown: filter_rp_grades.includes('brown'),
+                            fade_button: !filter_rp_grades.includes('brown')
+                        }">brown</span></label>
+                <label><input type="checkbox" name="black" value="black" v-model="filter_rp_grades" /><span
+                        class="toggle button" :class="{
+                            black: filter_rp_grades.includes('black'),
+                            fade_button: !filter_rp_grades.includes('black')
+                        }">black</span></label>
+            </div>
+        </div>
+        <label>Do you set problems?</label>
+        <div>
             <select v-model="setter" name="setter">
                 <option value="no">no</option>
                 <option value="yes">yes</option>
@@ -70,8 +120,17 @@ async function link_setter_code(e) {
 
 
 <style scoped>
+.search_options .button {
+    font-size: 0.65em;
+    margin-right: 0.2em;
+}
+
 button {
     width: 100%;
+}
+
+.fade_button {
+    background-color: hsl(207.9, 25%, 42.5%);
 }
 
 .labelblock {
