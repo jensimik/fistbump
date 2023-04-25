10ede08d83d5a84d708081ca560969694b0a5fcb
Jens Davidsen <jensimik@users.noreply.github.com>
2023-04-25 21:16:06 +0200

    .

--- backend/fistbump/routers/misc.py
+++ backend/fistbump/routers/misc.py
@@ -29,7 +29,14 @@ async def strip() -> schemas.Strip:
         (datetime(2023, 3, 23, 12, 0, 0), "probyg", "Section 2"),
         (datetime(2023, 3, 31, 12, 0, 0), "fribyg", "Section 3"),
         (datetime(2023, 4, 18, 12, 0, 0), "juniorbattle", "Section 1+2+3+4+5"),
-        (datetime(2023, 6, 6, 12, 0, 0), "no plan yet?", "Section ?"),
+        (datetime(2023, 5, 12, 12, 0, 0), "probyg", "Section 2"),
+        (datetime(2023, 5, 19, 12, 0, 0), "fribyg", "Section 1"),
+        (datetime(2023, 5, 25, 12, 0, 0), "probyg", "Section 3"),
+        (datetime(2023, 6, 2, 12, 0, 0), "fribyg", "Section 4"),
+        (datetime(2023, 6, 8, 12, 0, 0), "probyg", "Section 5"),
+        (datetime(2023, 6, 15, 12, 0, 0), "workshop", "Section 2"),
+        (datetime(2023, 6, 22, 12, 0, 0), "probyg", "Section 1"),
+        (datetime(2023, 7, 30, 12, 0, 0), "no plan yet?", "Section ?"),
     ]
     for next_strip, setters, section in STRIPDATA:
         if next_strip >= today:
