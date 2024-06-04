d276d744777bd441ae1edc87836d22477e7ac4ff
Jens Davidsen <jens@gnerd.dk>
2024-06-04 14:22:09 +0200

    Update ical.py

--- backend/fistbump/ical.py
+++ backend/fistbump/ical.py
@@ -24,7 +24,7 @@ async def get_calendar_agenda():
                 hour_start = event["start"].split("T")[1]
                 hour_end = event["end"].split("T")[1]
                 yield {
-                    "eventid": event["id"] + event["start"],
+                    "eventid": str(event["id"]) + event["start"],
                     "datetime": event["start"],
                     "date": event["start"][:10],
                     "time": " - ".join([hour_start, hour_end]),
